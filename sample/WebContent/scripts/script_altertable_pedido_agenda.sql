409dffa832d449e2bf96d1ef4790b956d59b9269
Bruno <brunomaia86@gmail.com>
2014-10-24 20:36:53 -0200

    Adicionei pedido a aba cadastro e criei script para modificação de banco. Ajuste Ajuste na tela de agendamento, pedido, menu

---
+++ sample/WebContent/scripts/script_altertable_pedido_agenda.sql
@@ -0,0 +1,14 @@
+﻿ ALTER TABLE agenda ADD COLUMN medico_id bigint;
+
+ 
+ALTER TABLE agenda
+
+  ADD CONSTRAINT fk_agenda_medico FOREIGN KEY (medico_id)
+      REFERENCES medico (pessoa_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+      ALTER TABLE pedido ADD COLUMN nome_cliente varchar(255);
+ 
+
+ 
+
